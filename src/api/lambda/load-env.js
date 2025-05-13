21d0c012536a7e2230d423ae0de799a78b5300a8
Joe Rong <jmrong@gmail.com>
2025-05-13 07:57:44 -0400

    Delete load-env.js

--- src/api/lambda/load-env.js
+++
@@ -1,7 +0,0 @@
-// load-env.js
-import dotenv from 'dotenv'
-dotenv.config({ path: '.env.local' })
-
-console.log('Environment variables loaded:')
-console.log('NODE_ENV:', process.env.NODE_ENV)
-console.log('PG_USER:', process.env.PG_USER)
